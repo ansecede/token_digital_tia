c0fc84205ecfc59dc4e8b2c9ec07844616d43281
ansecede <ansebascdno@gmail.com>
2025-02-16 00:29:15 -0500

    feat(web): Implementa nuevo layout y navbar, y actualiza la página de inicio.

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,13 +1,22 @@
-import "./styles.css";
+import { Metadata } from "next";
+import Navbar from "../components/navbar/Navbar";
+import "./globals.css";
+
+export const metadata: Metadata = {
+    title: "Token Digital Tia",
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className="bg-zinc-900 text-gray-200 min-h-screen">
+                <Navbar />
+                <main className="h-screen pt-20">{children}</main>
+            </body>
+        </html>
+    );
 }
---
+++ apps/web/src/components/navbar/Navbar.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { cn } from "@/src/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+export default function Navbar() {
+    const pathname = usePathname();
+
+    return (
+        <nav className="z-50 px-20 py-4 w-full fixed bg-zinc-900 border-b border-dark-gray/50">
+            <div className="flex items-center min-h-12 h-12 justify-evenly text-zinc-400">
+                <NavItem href={"/login"} pathname={pathname}>
+                    Cliente
+                </NavItem>
+                <NavItem href={"/token"} pathname={pathname}>
+                    Token
+                </NavItem>
+                <NavItem href={"/tabla"} pathname={pathname}>
+                    Tabla
+                </NavItem>
+            </div>
+        </nav>
+    );
+}
+
+type Props = { pathname: string } & React.ComponentProps<typeof Link>;
+
+function NavItem({ pathname, href, className, children, ...props }: Props) {
+    return (
+        <Link
+            data-selected={href === pathname}
+            className={cn(
+                "hover:text-zinc-100 data-[selected=true]:text-zinc-100 flex items-center text-lg font-medium transition-colors sm:text-base",
+                className
+            )}
+            href={href ?? "/"}
+            {...props}
+        >
+            {children}
+        </Link>
+    );
+}
